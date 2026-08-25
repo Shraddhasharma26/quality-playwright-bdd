c429aea95ba16ed59346c36d946be6a7cb6f3536
Shraddha <shraddhasharma6991@gmail.com>
2026-08-25 20:10:52 +0530

    Removed the comments from sortClass

--- src/pageObject/sortClass.js
+++ src/pageObject/sortClass.js
@@ -49,66 +49,52 @@ class sortClass extends baseClass
     }
     async atozPageDisplay()
     {
-      console.log("start of a-z")
     await this.page.waitForLoadState('domcontentloaded');
      const optionlist = (await this.page.locator('h4.title').allTextContents())
        .map((name) => name.trim())
      console.log(optionlist)
      const expectedNamesSorted = [...optionlist].sort((a, b) => a.localeCompare(b));
      expect(optionlist).toEqual(expectedNamesSorted);
-     console.log("end of a to z")
     }
      async optionZtoA()
-     {
-      console.log("start of option z to a")
+    {
       await this.sortBy.selectOption({label:'Name (Z - A)'})
-      console.log("end of option z to a")
      }
 
     async ztoapageDisplay()
     {
-      console.log("start of z to a")
       await this.page.waitForLoadState('domcontentloaded');
       const optionlist = (await this.page.locator('h4.title').allTextContents())
         .map((name) => name.trim())
      console.log(optionlist)
      const expectedNamesSorted = [...optionlist].sort((a, b) => b.localeCompare(a));
      expect(optionlist).toEqual(expectedNamesSorted);
-      console.log("end of z to a")
     }
     async optionLowtoHigh()
     {
-       console.log("start of low to high")
       await this.sortBy.selectOption({label:'Price (Low > High)'})
-       console.log("end of low to high")
     }
     async lowtoHighPageDisplay()
     {
-       console.log("start of low to high")
     await this.page.waitForLoadState('domcontentloaded');
       const optionlist = (await this.page.locator('span.price-new').allInnerTexts())
         .map(parsePrice)
      console.log(optionlist)
      const expectedNamesSorted = [...optionlist].sort((a, b) => a - b);
      expect(optionlist).toEqual(expectedNamesSorted);
-      console.log("end of low to high")
     }
     async optionHightoLow()
     {
-      console.log("start of high to low")
       await this.sortBy.selectOption({label:'Price (High > Low)'})
-      console.log("end of high to low")
     }
     async hightolowPageDisplay()
     {
-      console.log("start of display high to low")
     await this.page.waitForLoadState('domcontentloaded');
     const optionlist = (await this.page.locator('span.price-new').allTextContents())
       .map(parsePrice)
     console.log(optionlist)
     const expectedNamesSorted = [...optionlist].sort((a, b) => b - a)
     expect(optionlist).toEqual(expectedNamesSorted);
-    console.log("end of high to low ")
     }
   }
 
