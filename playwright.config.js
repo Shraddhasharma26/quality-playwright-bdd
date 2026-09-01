aa5d04d77ebad4dae1e2ea17dfddb16f5a0a061f
Ankit Sharma <shraddhasharma6991@gmail.com>
2026-09-02 00:02:40 +0530

    Changed config file

--- playwright.config.js
+++ playwright.config.js
@@ -8,22 +8,47 @@ const testDir = defineBddConfig({
 
 export default defineConfig({
   testDir,
-   timeout: 90_000,
+  timeout: 90_000,
   expect: { timeout: 10_000 },
   fullyParallel: false,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 4 : 1,
   reporter: [['list'], ['html', { open: 'never' }]],
-  use: 
-  {
-  
-    browserName:'chromium',
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium',
+        baseURL: 'https://ecommerce-playground.lambdatest.io/',
+        headless: process.env.CI ? true : false,
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
+        video: 'on-first-retry',
+        actionTimeout: 15_000,
+        navigationTimeout: 30_000,
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        browserName: 'firefox',
+        baseURL: 'https://ecommerce-playground.lambdatest.io/',
+        headless: process.env.CI ? true : false,
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
+        video: 'on-first-retry',
+        actionTimeout: 15_000,
+        navigationTimeout: 30_000,
+      },
+    },
+  ],
+  use: {
     baseURL: 'https://ecommerce-playground.lambdatest.io/',
-    headless:false,
+    headless: process.env.CI ? true : false,
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
     video: 'on-first-retry',
-     actionTimeout: 15_000,
+    actionTimeout: 15_000,
     navigationTimeout: 30_000,
   },
 });
